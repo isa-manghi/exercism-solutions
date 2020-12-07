55f3078f07316c8be74cbbc33586cbc30854782f
Isabella Manghi <isabellamanghi@gmail.com>
2020-12-07 17:16:38 +0000

    all tests pass

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -15,33 +15,23 @@ type Clock struct {
 // The purpose of constructor is to initialize the object of a class.
 // The purpose of a method is to perform a task in Go.
 func New(hours, minutes int) Clock {
-	// create a clock Object
-	//minutes += hours * 60 // ensures minutes rolls into hours
-	//minutes /= (24 * 60)  // gives a day frame and ensures it stays within 24 hours
-	//if minutes < 0 {
-	//	minutes += 60 * 24 // ensures that negative minutes are converted into appropriate time
-	//}
 	// - if minutes are greater than or equal to 60, must roll into hours
 	// - if hours are equal or greater than 24, must start from 00:00
 	// - if minutes or hours are negative then they must be converted into positive time
 
-
-	//hoursinday := 24
-	//minutesinday := 1440
-	//minutesinhour := 60
-
+	for minutes < 0 { // if minutes is less than zero, then add 60 mins to make it positive
+		minutes += 60 // add 60 mins until it is more than 0
+		hours -- // minuses one hour with each iteration as it is a negative value
+	}
+	for hours < 0 { // if hours is less than zero, then add 24 to make it positive
+		hours += 24
+	}
 	for minutes >= 60 {
 		minutes -= 60 // minus 60 in each loop until it is less than 60
 		hours++ // adds one to each iteration
 	}
 	if hours >= 24 {
 		hours %= 24 //remainder to roll into hours
-	}
-	if minutes < 0 { // if minutes is less than zero, then add 60 mins to make it positive
-		minutes += 60
-	}
-	if hours < 0 { // if hours is less than zero, then add 24 to make it positive
-		hours += 24
 	}
 	return Clock{hours: hours, minutes: minutes}
 }
@@ -56,10 +46,12 @@ func (c Clock) String() string {
 
 // Add minutes to time
 func (c Clock) Add(minutes int) Clock {
-	return New(c.hours, c.minutes+minutes)
+	c.minutes += minutes
+	return New(c.hours, c.minutes)
 }
 
 // Subtract minutes from time
 func (c Clock) Subtract(minutes int) Clock {
-	return New(c.hours, c.minutes-minutes)
+	c.minutes -= minutes
+	return New(c.hours, c.minutes)
 }
